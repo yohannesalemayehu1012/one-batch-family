d256c7cca6ee59bddacff2d2d63f561f4944da55
yohannesalemayehu1012 <yohannesalemayehu1012@gmail.com>
2026-08-03 14:40:50 +0300

    Configure GitHub deployment

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 // Application configuration
 const APP_NAME = 'One Batch Family';
-const BASE_URL = 'https://one-batch.infinityfree.me/';
+const BASE_URL = '/sql110.infinityfree.com';
 const DEFAULT_PAGE = 'home';
--- config/database.php
+++ config/database.php
@@ -2,12 +2,6 @@
 // Database connection settings
 return [
     'host' => 'sql308.infinityfree.com',
-// 'host'     => 'localhost',
-// 'username' => 'root',
-// 'password' => '',
-// 'database' => 'one_batch_family',
-// 'charset'  => 'utf8mb4',
-
     'username' => 'if0_42562407',
     'password' => 'bF8udNcW17D8ID',
     'database' => 'if0_42562407_abtestdb007',
